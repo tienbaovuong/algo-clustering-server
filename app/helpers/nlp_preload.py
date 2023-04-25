ad091ab37255cc34ee20eba5d197d485f8711bca
Bao <v.baovt2@vinbigdata.com>
2023-04-25 21:21:36 +0700

    nlp service

--- app/helpers/nlp_preload.py
+++ app/helpers/nlp_preload.py
@@ -6,7 +6,7 @@ import underthesea
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from transformers import AutoModel, AutoTokenizer, T5ForConditionalGeneration, T5Tokenizer
+from transformers import AutoModel, AutoTokenizer
 
 class NLP():
     phobert: Any = None
@@ -15,11 +15,30 @@ class NLP():
 
     def initialize(self):
         # Load model
-        self.phobert = AutoModel.from_pretrained("vinai/phobert-base")
-        self.tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base")
+        self.phobert = AutoModel.from_pretrained("vinai/phobert-base-v2")
+        self.tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
 
         # Stop word
         fname = 'vn_stopword.txt'
         self.stop_word_data = np.genfromtxt(fname, dtype='str', delimiter='\n', encoding="utf8").tolist()
+
+    def extract_feature(self, lines):
+        if not self.phobert or not self.tokenizer:
+            return None
+        features_list = []
+        for line in lines:
+            # Preprocess
+            line = gensim.utils.simple_preprocess(str(line))
+            line = " ".join(line)
+            # Segmentation
+            line = underthesea.word_tokenize(line, format="text")
+            line = " ".join(line)
+            # Extract feature
+            input_ids = torch.tensor([self.tokenizer.encode(line)])
+
+            with torch.no_grad():
+                features = self.phobert(input_ids)
+            features_list.append(features)
+        return features_list
 
 nlp_service = NLP()
